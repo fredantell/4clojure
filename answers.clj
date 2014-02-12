f55f9e6448d7a5d01f8ca0de12b890328a86b0fe
Fred Antell <fred@fredantell.com>
2014-02-12 15:15:40 -0400

    Problem 8

--- answers.clj
+++ answers.clj
@@ -72,5 +72,13 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 8 - Intro to Sets
+
+;; Sets are collections of unique values.
+
+;; (= __ (set '(:a :a :b :c :c :c :c :d :d)))
+;; (= __ (clojure.set/union #{:a :b :c} #{:b :c :d}))
+
+;; #{:a :b :c :d}
 
 
