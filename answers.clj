f537b99ba5d17b9c76ee73d584f252a662f462b6
Fred Antell <fred@fredantell.com>
2014-02-12 14:46:44 -0400

    Problem 5

--- answers.clj
+++ answers.clj
@@ -37,3 +37,13 @@
 
 ;; :a :b :c
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Problem 5 - Lists:conj
+
+;; When operating on a list, the conj function will return a new list with one or more items "added" to the front.
+
+;; (= __ (conj '(2 3 4) 1))
+;; (= __ (conj '(3 4) 2 1))
+
+;; '(1 2 3 4)
