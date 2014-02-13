acdf61fb23249c87758d9085edd5a94b8c0a5fad
Fred Antell <fred@fredantell.com>
2014-02-13 11:36:36 -0400

    Problem 12

--- answers.clj
+++ answers.clj
@@ -112,6 +112,21 @@
 
 ;; {:b 2}
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Problem 12 - Intro to Sequences
+
+;; All Clojure collections support sequencing. You can operate on sequences with functions like first, second, and last.
+
+;; (= __ (first '(3 2 1)))
+;; (= __ (second [2 3 4]))
+;; (= __ (last (list 1 2 3)))
+
+;; 3
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 
 
 
