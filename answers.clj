865aefa933666bd761c41eb24221495eccfbbcd0
Fred Antell <fred@fredantell.com>
2014-02-13 11:28:40 -0400

    Problem 11

--- answers.clj
+++ answers.clj
@@ -104,6 +104,18 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 11 Maps:conj
+
+;; When operating on a map, the conj function returns a new map with one or more key-value pairs "added".
+
+;; (= {:a 1, :b 2, :c 3} (conj {:a 1} __ [:c 3]))
+
+;; {:b 2}
+
+
+
+
+
 
 
 
