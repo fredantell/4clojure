d84ebd9ad915b1286147bdd6ed77ca32e3c1bede
Fred Antell <fred@fredantell.com>
2014-02-14 14:21:30 -0400

    Problem 18

--- answers.clj
+++ answers.clj
@@ -187,3 +187,13 @@
 ;; [6 7 8]
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Problem 18 - Sequenecs: filter
+
+;; The filter function takes two arguments: a predicate function (f) and a sequence (s). Filter returns a new sequence consisting of all the items of s for which (f item) returns true.
+
+;; (= __ (filter #(> % 5) '(3 4 5 6 7)))
+
+;; [6 7]
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
