1384aba162b32e182fe48bd27ebe7e5202d11a35
Fred Antell <fred@fredantell.com>
2014-02-17 12:33:07 -0400

    Problem 21

--- answers.clj
+++ answers.clj
@@ -255,6 +255,26 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 21 - Nth Element
+
+;; Write a function which returns the Nth element from a sequence.
+;; Special Restrictions: nth
+
+;;test cases
+(comment
+  (= (__ '(4 5 6 7) 2) 6)
+  (= (__ [:a :b :c] 0) :a)
+  (= (__ [1 2 3 4] 1) 2)
+  (= (__ '([1 2] [3 4] [5 6]) 2) [5 6]))
+
+;;answer
+(comment
+  (fn [seq pos]
+    (last (take (+ pos 1) seq)))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Problem 35 - Local Bindings
 
 ;; Clojure lets you give local names to values using the special let-form.
@@ -266,3 +286,5 @@
 ;; 7
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
