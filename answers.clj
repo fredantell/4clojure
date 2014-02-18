ad41d284b96f597179c23e8d734327425d7d0471
Fred Antell <fred@fredantell.com>
2014-02-18 12:56:10 -0400

    Problem 24

--- answers.clj
+++ answers.clj
@@ -296,6 +296,22 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 24 - Sum It All Up
+
+;; Write a function which returns the sum of a sequence of numbers.
+
+(comment 
+  (= (__ [1 2 3]) 6)
+  (= (__ (list 0 -2 5 5)) 8)
+  (= (__ #{4 2 1}) 7)
+  (= (__ '(0 0 -1)) -1)
+  (= (__ '(1 10 3)) 14)
+)
+
+;; #(reduce + %)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Problem 35 - Local Bindings
 
 ;; Clojure lets you give local names to values using the special let-form.
