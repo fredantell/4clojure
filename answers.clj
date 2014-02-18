d7d638ec051f33666cab437b16e281a5068ef46c
Fred Antell <fred@fredantell.com>
2014-02-18 13:38:30 -0400

    Problem 25

--- answers.clj
+++ answers.clj
@@ -312,6 +312,23 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 25 - Find the Odd Numbers
+
+;; Write a function which returns only the odd numbers from a sequence.
+
+(comment
+  (= (__ #{1 2 3 4 5}) '(1 3 5))
+  (= (__ [4 2 1 6]) '(1))
+  (= (__ [2 2 4 6]) '())
+  (= (__ [1 1 1 3]) '(1 1 1 3))
+)
+
+(comment
+  #(filter (fn [x] (= 1 (mod x 2))) %)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Problem 35 - Local Bindings
 
 ;; Clojure lets you give local names to values using the special let-form.
