defb4f8e1d142a38889ffb8435e81ba3db9a47f1
Fred Antell <fred@fredantell.com>
2014-03-03 11:34:33 -0400

    Problem 134

--- answers.clj
+++ answers.clj
@@ -534,4 +534,21 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 134 - A nil key
+
+;; Write a function which, given a key and map, returns true iff the map contains an entry with that key and its value is nil.
+
+(comment 
+  (true?  (__ :a {:a nil :b 2}))
+  (false? (__ :b {:a nil :b 2}))
+  (false? (__ :c {:a nil :b 2}))
+)
+(comment 
+  (fn [key map]
+    (and (contains? map key) (nil? (key map))))
+
+  #(nil? (get %2 % "none")) ;; alternate solution
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
