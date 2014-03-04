b9d3babdd813ae6f97ab5610ddcbc8a11484afb0
Fred Antell <fred@fredantell.com>
2014-03-04 15:11:06 -0400

    Problem 48

--- answers.clj
+++ answers.clj
@@ -483,6 +483,19 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 48 - Intro to Some
+
+;; The some function takes a predicate function and a collection. It returns the first logical true value of (predicate x) where x is an item in the collection.
+
+(comment 
+  (= __ (some #{2 7 6} [5 6 7 8]))
+  (= __ (some #(when (even? %) %) [5 6 7 8]))
+)
+
+;; 6
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Problem 57 - Simple Recursion
 
 ;; A recursive function is a function which calls itself. This is one of the fundamental techniques used in functional programming.
