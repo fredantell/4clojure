a3766e372eb1bd039c8953eb10f3232b6f377dd9
Fred Antell <fred@fredantell.com>
2014-03-10 11:31:12 -0400

    Problem 162

--- answers.clj
+++ answers.clj
@@ -688,3 +688,21 @@
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Problem 162 - Logical falsity and truth
+
+;; In Clojure, only nil and false represent the values of logical falsity in conditional tests - anything else is logical truth.
+
+(comment 
+  (= __ (if-not false 1 0))
+  (= __ (if-not nil 1 0))
+  (= __ (if true 1 0))
+  (= __ (if [] 1 0))
+  (= __ (if [0] 1 0))
+  (= __ (if 0 1 0))
+  (= __ (if 1 1 0))
+)
+
+;; 1  ;; these all equal 1
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
