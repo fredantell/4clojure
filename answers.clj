4f92b33bf00e7c8132b9711fb4a4a3703ca1cfd3
Fred Antell <fred@fredantell.com>
2014-03-12 11:28:19 -0400

    Problem 31

--- answers.clj
+++ answers.clj
@@ -458,6 +458,22 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 31 - Pack a Sequence
+
+;; Write a function which packs consecutive duplicates into sub-lists.
+
+(comment 
+  (= (__ [1 1 2 1 1 1 3 3]) '((1 1) (2) (1 1 1) (3 3)))
+  (= (__ [:a :a :b :b :c]) '((:a :a) (:b :b) (:c)))
+  (= (__ [[1 2] [1 2] [3 4]]) '(([1 2] [1 2]) ([3 4])))
+)
+
+(comment 
+  #(partition-by identity %)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Problem 32 - Duplicate a Sequence
 
 ;; Write a function which duplicates each element of a sequence.
