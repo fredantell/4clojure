5af632459df466e037de03cf4783a88ed0c8622b
Fred Antell <fred@fredantell.com>
2014-05-17 12:04:19 -0400

    Problem 90

--- answers.clj
+++ answers.clj
@@ -1020,6 +1020,30 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 90 - Cartesian Product
+
+;; Write a function which calculates the Cartesian product of two sets.
+
+(comment 
+  (= (__ #{"ace" "king" "queen"} #{"♠" "♥" "♦" "♣"})
+     #{["ace"   "♠"] ["ace"   "♥"] ["ace"   "♦"] ["ace"   "♣"]
+       ["king"  "♠"] ["king"  "♥"] ["king"  "♦"] ["king"  "♣"]
+       ["queen" "♠"] ["queen" "♥"] ["queen" "♦"] ["queen" "♣"]})
+
+  (= (__ #{1 2 3} #{4 5})
+     #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})
+
+  (= 300 (count (__ (into #{} (range 10))
+                    (into #{} (range 30)))))
+)
+
+(comment 
+  #(into #{} (for [x % y %2]
+             [x y]))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Problem 99 - Product Digits
 
 ;; Write a function which multiplies two numbers and returns the result as a sequence of its digits.
