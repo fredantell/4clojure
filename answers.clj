0a845130c70a2bace15c62abd8f674c25c800781
Fred Antell <fred@fredantell.com>
2014-05-18 12:57:36 -0400

    Problem 55

--- answers.clj
+++ answers.clj
@@ -839,6 +839,25 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 55 - Count Occurrences
+
+;; Write a function which returns a map containing the number of occurences of each distinct item in a sequence.
+;; Special Restrictions: frequencies
+
+(comment 
+  (= (__ [1 1 2 3 2 1 1]) {1 4, 2 2, 3 1})
+  (= (__ [:b :a :b :a :b]) {:a 2, :b 3})
+  (= (__ '([1 2] [1 3] [1 3])) {[1 2] 1, [1 3] 2})
+)
+
+(comment 
+ #(reduce (fn [a b] (assoc a b (inc (get a b 0))))
+          {}
+          %)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Problem 57 - Simple Recursion
 
 ;; A recursive function is a function which calls itself. This is one of the fundamental techniques used in functional programming.
