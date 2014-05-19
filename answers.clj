5028ab359240c8641c54f27e59e3c26ad2b1f74e
Fred Antell <fred@fredantell.com>
2014-05-19 15:12:08 -0400

    Problem 58

--- answers.clj
+++ answers.clj
@@ -893,6 +893,27 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 58 - Function Composition
+
+;; Write a function which allows you to create function compositions. The parameter list should take a variable number of functions, and create a function applies them from right-to-left.
+;; Special Restrictions: comp
+
+(comment 
+  (= [3 2 1] ((__ rest reverse) [1 2 3 4]))
+  (= 5 ((__ (partial + 3) second) [1 2 3 4]))
+  (= true ((__ zero? #(mod % 8) +) 3 5 7 9))
+  (= "HELLO" ((__ #(.toUpperCase %) #(apply str %) take) 5 "hello world"))
+)
+
+(comment
+  (fn [& fns]
+    (fn [& args]
+      (let [[f & fns] (reverse fns)]
+        (reduce #(%2 %1) (apply f args) fns))))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Problem 61 - Map Construction
 
 ;; Write a function which takes a vector of keys and a vector of
