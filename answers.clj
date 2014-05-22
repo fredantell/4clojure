8b7acfeeff47c7a6b04899f646700946393263b3
Fred Antell <fred@fredantell.com>
2014-05-22 13:35:40 -0400

    Problem 54

--- answers.clj
+++ answers.clj
@@ -839,6 +839,27 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 54 - Partition a Sequence
+
+;; Write a function which returns a sequence of lists of x items each. Lists of less than x items should not be returned.
+;Special Restrictions: partition partition-all
+
+(comment 
+  (= (__ 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8)))
+  (= (__ 2 (range 8)) '((0 1) (2 3) (4 5) (6 7)))
+  (= (__ 3 (range 8)) '((0 1 2) (3 4 5)))
+)
+
+(comment 
+(fn part* [n xs]
+   (let [h (take n xs)
+         t (drop n xs)]
+     (if (>= (count xs) n)
+       (cons h (part* n t)))))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Problem 55 - Count Occurrences
 
 ;; Write a function which returns a map containing the number of occurences of each distinct item in a sequence.
