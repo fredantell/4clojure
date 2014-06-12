6af12be5eebcd06585d1aaa6427fd67dbcc3f9ab
Fredrik Antell <fred@fredantell.com>
2014-06-11 19:53:49 -0700

    Problem 95

--- answers.clj
+++ answers.clj
@@ -1254,6 +1254,52 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Problem 95 - To Tree or Not to Tree
+
+;; Write a predicate which checks whether or not a given sequence represents a binary tree. Each node in the tree must have a value, a left child, and a right child.
+
+(comment 
+  (= (__ '(:a (:b nil nil) nil))
+     true)
+
+  (= (__ '(:a (:b nil nil)))
+     false)
+
+  (= (__ [1 nil [2 [3 nil nil] [4 nil nil]]])
+     true)
+
+  (= (__ [1 [2 nil nil] [3 nil nil] [4 nil nil]])
+     false)
+
+  (= (__ [1 [2 [3 [4 nil nil] nil] nil] nil])
+     true)
+
+  (= (__ [1 [2 [3 [4 false nil] nil] nil] nil])
+     false)
+
+  (= (__ '(:a nil ()))
+     false)
+)
+
+(comment
+  ;; You're checking for two things.  The item you're examining is either
+  ;; going to be nil, which signals that the node terminates there.  Or,
+  ;; you're going to see something that is: a sequence, has 3 items,
+  ;; and those items also pass these tests when examined recursively.
+  ;; The only other thing to be aware of with this solution is how short-circuiting
+  ;; works.  It's concise but perhaps not always the clearest to
+  ;; exploit or/and this way.
+  (fn tree? [x]
+  (or (nil? x)
+      (and
+       (sequential? x)
+       (= 3 (count x))
+       (tree? (second x))
+       (tree? (last x)))))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Problem 97 - Pascal's Triangle
 
 ;; Pascal's triangle is a triangle of numbers computed using the following rules:
